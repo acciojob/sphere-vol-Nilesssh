f8addac17fee25d76e4b8a6c74a995bbb6a749a0
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-24 01:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const radiusData = document.querySelector("#radius")
 const volumeData = document.querySelector("#volume")
+const radiusData = document.querySelector("#radius")
+const passage = document.querySelector("#passage")
 function volume_sphere() {
     //Write your code here
 	event.preventDefault();
-	volumeData.value = (4/3)*Math.PI*Math.pov(radiusData.value, 3).toFixed(4);
+	volumeData.value = ((4/3)*Math.PI*Math.pow(radiusData.value, 3)).toFixed(4);
+  
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
